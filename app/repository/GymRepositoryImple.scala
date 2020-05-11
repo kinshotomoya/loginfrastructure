723bbc661f9f9a137947b31e4ac3a84de4b0fdc4
金正朋也 <mhtnim1023@gmail.com>
2020-05-11 14:49:02 +0900

    add 上限境界コメント

--- app/repository/GymRepositoryImple.scala
+++ app/repository/GymRepositoryImple.scala
@@ -7,6 +7,8 @@ import repository.models.gym.MysqlSearchRequest
 import scala.concurrent.Future
 
 class GymRepositoryImple extends GymRepository{
+  // T <: StringBaseと上限境界を設けることで、request.value.nameとStringBaseのプロパティにアクセスできるようになる
+  // Tだけだと、どんな型が来るのかわからないので、引数のプロパティにアクセスすることはできない
    override def search[T <: StringBase](request: MysqlSearchRequest[T]): Future[String] = {
     Future.successful(request.value.name)
   }
