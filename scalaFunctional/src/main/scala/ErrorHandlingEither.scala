cc51732f30630abfd154fe279fa4b3ac3db0db11
kinsho-stb <tomoya.kinsho@stanby.inc>
2020-09-30 09:56:29 +0900

    add exercise 4.7

--- scalaFunctional/src/main/scala/ErrorHandlingEither.scala
+++ scalaFunctional/src/main/scala/ErrorHandlingEither.scala
@@ -2,7 +2,7 @@
 object ErrorHandlingEither {
 
 
-  sealed trait Either[+E, +A]
+//  sealed trait Either[+E, +A]
   case class Left[+E](value: E) extends Either[E, Nothing] {
     override def equals(that: Any): Boolean = ???
   }
@@ -10,8 +10,8 @@ object ErrorHandlingEither {
     override def equals(that: Any): Boolean = ???
   }
 
-  // exercise 4.7
-  trait Either[+E, +A] {
+  // exercise 4.6
+  sealed trait Either[+E, +A] {
     def map[B](f: A => B): Either[E, B] = {
       this match {
         case Left(value) => Left(value)
@@ -25,6 +25,32 @@ object ErrorHandlingEither {
         case Right(value) => f(value)
       }
     }
+
+    def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = {
+      this match {
+        case Left(_) => b
+        case Right(_) => this
+      }
+    }
+
+    def map2[EE >: E, B , C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+      this match {
+        case l @ Left(_) => l
+        case Right(value) => b match {
+          case l @ Left(_) => l
+          case Right(value2) => Right(f(value, value2))
+        }
+      }
+    }
+
+    def map22[EE >: E, B , C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+      this.flatMap(x => b.map(y => f(x, y)))
+    }
+
+
+    // exercise 4.7
+
+
   }
 
 }
